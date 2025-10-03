08600a0dacaeecbe8510b97acf6cab6466444db9
Maulik Dave <maulikdave2005@gmail.com>
2025-10-03 23:36:08 +0530

    Backend Sorted

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV != "production") {
-  require("dotenv").config();
-}
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
 import { createServer } from "node:http";
