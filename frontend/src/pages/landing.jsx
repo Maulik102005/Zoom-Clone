0165923964b2c290ab7ea899b5cf60a6947ce76f
Maulik Dave <maulikdave2005@gmail.com>
2025-10-19 02:50:08 +0530

    Creating Landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,5 +1,41 @@
 import React from "react";
 
 export default function LandingPage() {
-  return <div className="LandingPageContainer"></div>;
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>Video Call</h2>
+        </div>
+        <div className="navList">
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <div role="button">
+            {" "}
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+
+      <div className="landingMainContainer">
+        <div>
+          <h1>
+            <span style={{ color: "#FF9839" }}>Connect</span> with your loved
+            ones
+          </h1>
+          <p>
+            Experience seamless video calls with our user-friendly platform.
+          </p>
+
+          <div role="button" className="getStartedButton">
+            Get Started
+          </div>
+        </div>
+
+        <div>
+          <img src="/public/mobile.png" alt="mobile" />
+        </div>
+      </div>
+    </div>
+  );
 }
